d5405627bb1696b7f33f2338c2ab8b1e8e94e88b
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-23 20:25:24 +0600

    Initial project of servicia setup compleate.

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <p>Hi</p>,
+    },
+]);
+
+export default router;
