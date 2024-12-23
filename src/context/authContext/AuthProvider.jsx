6373f6e1d4199643bcd8ac532bb113dd0b9b404a
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-23 22:19:51 +0600

    Context setup compleate.

---
+++ src/context/authContext/AuthProvider.jsx
@@ -0,0 +1,11 @@
+import AuthContext from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+    const data = {
+        name: "John Doe",
+    };
+
+    return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
