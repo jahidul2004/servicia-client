cc4f3fcb92b044a7207e94c3ce7dd0d90167b83b
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-24 11:25:28 +0600

    Featured card section added.

--- src/pages/featuredService/FeaturedService.jsx
+++ src/pages/featuredService/FeaturedService.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import FeaturedServiceCard from "./FeaturedServiceCard";
+import { use } from "react";
+import axios from "axios";
 
 const FeaturedService = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/services?limit=6").then((res) => {
+            setServices(res.data);
+        });
+    }, []);
     return (
         <div className="mx-4">
-            <h1 className="text-3xl font-bold text-center py-4 text-[#357ef0]">Featured Services!</h1>
+            <h1 className="text-3xl font-bold text-center py-4 text-[#357ef0]">
+                Featured Services!
+            </h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <FeaturedServiceCard></FeaturedServiceCard>
+                {services.map((service) => (
+                    <FeaturedServiceCard
+                        key={service._id}
+                        serviceImage={service.serviceImage}
+                        serviceTitle={service.serviceTitle}
+                        description={service.description}
+                        price={service.price}
+                    ></FeaturedServiceCard>
+                ))}
             </div>
         </div>
     );
--- src/pages/featuredService/FeaturedServiceCard.jsx
+++ src/pages/featuredService/FeaturedServiceCard.jsx
@@ -1,20 +1,22 @@
-const FeaturedServiceCard = () => {
+const FeaturedServiceCard = ({
+    serviceImage,
+    serviceTitle,
+    description,
+    price,
+}) => {
     return (
         <div className="border-2 rounded-lg p-4">
-            <div className="bg-[#f1f1f1] p-2 rounded-lg">
+            <div className="bg-[#f1f1f1] p-2 rounded-lg h-[250px]">
                 <img
-                    className="rounded-lg"
-                    src="https://i.ibb.co.com/Gp5csvM/8-FC6545-D865-C150668-D5-A2-A02-C13-C732.webp"
+                    className="rounded-lg w-full h-full"
+                    src={serviceImage}
                     alt=""
                 />
             </div>
             <div className="mt-4">
-                <h1 className="text-2xl font-bold">Title Here</h1>
-                <p className="font-semibold my-2">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Optio, quisquam.
-                </p>
-                <p className="font-bold">Price: 5000 $</p>
+                <h1 className="text-2xl font-bold">{serviceTitle}</h1>
+                <p className="font-semibold my-2">{description}</p>
+                <p className="font-bold">Price: {price} $</p>
                 <button className="btn bg-[#357ef0] text-white mt-4">
                     See Details
                 </button>
