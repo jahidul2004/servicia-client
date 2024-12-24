ed3159a9e9b7798c6540987b0af0ee25f5eb5b3e
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-25 01:18:55 +0600

    Home stat updated.

--- src/pages/homeStat/HomeStat.jsx
+++ src/pages/homeStat/HomeStat.jsx
@@ -1,15 +1,15 @@
 const HomeStat = () => {
     return (
         <div className="bg-[#357ef0] p-4 py-10 flex flex-col md:flex-row justify-center md:justify-around text-white text-center gap-4 my-10">
-            <div className="w-[180px] h-[180px] rounded-full border flex flex-col items-center justify-center">
+            <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
                 <h1 className="text-4xl font-bold">510+</h1>
                 <p className="font-semibold text-xl">users</p>
             </div>
-            <div className="w-[180px] h-[180px] rounded-full border flex flex-col items-center justify-center">
+            <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
                 <h1 className="text-4xl font-bold">5120+</h1>
                 <p className="font-semibold text-xl">services</p>
             </div>
-            <div className="w-[180px] h-[180px] rounded-full border flex flex-col items-center justify-center">
+            <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
                 <h1 className="text-4xl font-bold">310+</h1>
                 <p className="font-semibold text-xl">reviews</p>
             </div>
