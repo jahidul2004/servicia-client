ef12648446e0ae771e667fc7c8a0859cb2d21037
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-24 21:36:15 +0600

    Search option added in services page.

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -11,9 +11,27 @@ const Services = () => {
             <Helmet>
                 <title>Servicia | Services</title>
             </Helmet>
-            <h1 className="text-3xl font-bold text-center text-[#357ef0] py-4">
-                All Services <br /> --------------------------
-            </h1>
+            <div className="flex flex-col items-center mb-5">
+                <h1 className="text-3xl font-bold text-center text-[#357ef0] py-4">
+                    All Services <br /> --------------------------
+                </h1>
+
+                <label className="input input-bordered border-[#357ef0] flex items-center gap-2">
+                    <input type="text" className="grow" placeholder="Search" />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </label>
+            </div>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 {data.map((service) => (
