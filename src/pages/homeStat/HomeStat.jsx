6f77295f887f9aa17a88a41cbcfc199adc8fa43c
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-25 16:55:47 +0600

    Dynamic home stat section updated.

--- src/pages/homeStat/HomeStat.jsx
+++ src/pages/homeStat/HomeStat.jsx
@@ -1,16 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const HomeStat = () => {
+    const [count, setCount] = useState({});
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/countData").then((res) => {
+            setCount(res.data);
+        });
+    }, []);
+
+    console.log(count);
     return (
         <div className="bg-[#357ef0] p-4 py-10 flex flex-col md:flex-row justify-center md:justify-around text-white text-center gap-4 my-10">
             <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold">510+</h1>
+                <h1 className="text-4xl font-bold">{count.userCount}</h1>
                 <p className="font-semibold text-xl">users</p>
             </div>
             <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold">5120+</h1>
+                <h1 className="text-4xl font-bold">{count.serviceCount}</h1>
                 <p className="font-semibold text-xl">services</p>
             </div>
             <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold">310+</h1>
+                <h1 className="text-4xl font-bold">{count.reviewCount}</h1>
                 <p className="font-semibold text-xl">reviews</p>
             </div>
         </div>
