dbd9108be4bbaf65917915bc6d47999438c9ac47
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-11 21:47:48 +0600

    Stat section responsive issue fixed.

--- src/pages/homeStat/HomeStat.jsx
+++ src/pages/homeStat/HomeStat.jsx
@@ -5,13 +5,15 @@ const HomeStat = () => {
     const [count, setCount] = useState({});
 
     useEffect(() => {
-        axios.get("https://servicia-server.vercel.app/countData").then((res) => {
-            setCount(res.data);
-        });
+        axios
+            .get("https://servicia-server.vercel.app/countData")
+            .then((res) => {
+                setCount(res.data);
+            });
     }, []);
 
     return (
-        <div className="bg-[#357ef0] p-4 py-10 flex flex-col md:flex-row justify-center md:justify-around text-white text-center gap-4 my-10">
+        <div className="bg-[#357ef0] p-4 py-10 flex flex-col md:flex-row justify-center md:justify-around text-white text-center gap-4 my-10 items-center">
             <div className="w-[180px] h-[180px] rounded-full border-2 flex flex-col items-center justify-center">
                 <h1 className="text-4xl font-bold">{count.userCount}</h1>
                 <p className="font-semibold text-xl">users</p>
